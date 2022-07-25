ad2c5601b491fd2e2f8c8d269ea4be502e2f0293
vmitra <vmitra@adobe.com>
2022-07-25 14:09:53 -0400

    3_commit

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,35 +1,18 @@
-/*
- * Copyright 2021 Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy
- * of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
+import { createOptimizedPicture } from '../../scripts/scripts.js';
 
-// eslint-disable-next-line import/no-unresolved
-import { createTag } from '../../scripts/scripts.js';
-
-export default function decorate($block) {
-  $block.querySelectorAll(':scope>div').forEach(($card) => {
-    $card.classList.add('card');
-    const $cardDivs = [...$card.children];
-    $cardDivs.forEach(($div) => {
-      if ($div.querySelector('img')) {
-        $div.classList.add('card-image');
-      } else {
-        $div.classList.add('card-content');
-      }
-      const $a = $div.querySelector('a');
-      if ($a && $a.textContent.startsWith('https://')) {
-        const $wrapper = createTag('a', { href: $a.href, class: 'card' });
-        $a.remove();
-        $wrapper.innerHTML = $card.innerHTML;
-        $block.replaceChild($wrapper, $card);
-      }
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    li.innerHTML = row.innerHTML;
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
     });
+    ul.append(li);
   });
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
 }
